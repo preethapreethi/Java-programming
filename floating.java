42c916150d5b53bd38d1fc4077d31e956aeee751
preethapreethi <43059527+preethapreethi@users.noreply.github.com>
2018-09-07 12:18:35 +0530

    Add files via upload

---
+++ floating.java
@@ -0,0 +1,16 @@
+lass Main {
+  public static void main(String[] args){
+    int a;
+   double b,c,d,e,f;
+    a=12;
+    b=22;
+    c=a+b;
+    d=a-b;
+    e=a/b;
+    f=a*b;
+    System.out.println("add value:" +c);
+    System.out.println("sub value:" +d);
+    System.out.println("multiply value:" +e);
+    System.out.println("divided value:" +f );
+  }
+}
